73dc8fbf79bf4aec9d35cc001fec29dbc9dbe9c3
Acássio Monteiro <montteiro.dsgn@gmail.com>
2022-06-29 16:54:55 -0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+Class Veiculo {
+
+    var $marca;
+    var $modelo;
+    var $ano;
+    var $cor;
+    var $valorDiaria;
+    var $alugado;
+    var $_ tipo (Passeio, Utilitario)
+
+}
+
+Class Passeio {
+    var $
+}
